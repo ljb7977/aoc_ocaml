07bd85e309e707169a8e780da4534fa6612aac61
Jubeen Lee <ljb7977@gmail.com>
2022-07-01 00:28:51 +0900

    Solved 2022 day2-2

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,6 @@
 let file = "resource/2021/day2.input"
 
-type command = Forward of int | Backward of int | Up of int | Down of int
+type command = Forward of int | Up of int | Down of int
 type coord = { x : int; y : int }
 
 let read_lines name : string list =
@@ -24,7 +24,6 @@ let parse_line l =
       let amount = int_of_string amount in
       match cmd with
       | "forward" -> Forward amount
-      | "backward" -> Backward amount
       | "up" -> Up amount
       | "down" -> Down amount
       | _ -> raise (E "Unknown command"))
@@ -34,7 +33,6 @@ let apply_command coord command =
   let { x; y } = coord in
   match command with
   | Forward a -> { x = x + a; y }
-  | Backward a -> { x = x - a; y }
   | Up a -> { x; y = y + a }
   | Down a -> { x; y = y - a }
 
@@ -43,6 +41,27 @@ let () =
   let commands = List.map parse_line lines in
   let final_coord = List.fold_left apply_command { x = 0; y = 0 } commands in
   let { x; y } = final_coord in
+  let product = x * y in
+  let t = abs product in
+  print_int t;
+  print_newline ()
+
+type aim_and_coord = { x : int; y : int; aim : int }
+
+let apply_command_2 coord command =
+  let { aim; x; y } = coord in
+  match command with
+  | Down a -> { x; y; aim = aim + a }
+  | Up a -> { x; y; aim = aim - a }
+  | Forward a -> { x = x + a; y = y + (aim * a); aim }
+
+let () =
+  let lines = read_lines file in
+  let commands = List.map parse_line lines in
+  let final_coord =
+    List.fold_left apply_command_2 { x = 0; y = 0; aim = 0 } commands
+  in
+  let { x; y; _ } = final_coord in
   let product = x * y in
   let t = abs product in
   print_int t;
